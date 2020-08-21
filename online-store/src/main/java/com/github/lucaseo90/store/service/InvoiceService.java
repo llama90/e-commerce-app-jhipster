90fc3460b17115a17e51da2f889691d65cbce244
Hyunseok Seo <hsseo0501@gmail.com>
2020-08-21 18:22:45 +0900

    Added limiting access to the data of other users

--- online-store/src/main/java/com/github/lucaseo90/store/service/InvoiceService.java
+++ online-store/src/main/java/com/github/lucaseo90/store/service/InvoiceService.java
@@ -2,6 +2,8 @@ package com.github.lucaseo90.store.service;
 
 import com.github.lucaseo90.store.domain.Invoice;
 import com.github.lucaseo90.store.repository.InvoiceRepository;
+import com.github.lucaseo90.store.security.AuthoritiesConstants;
+import com.github.lucaseo90.store.security.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,7 +49,11 @@ public class InvoiceService {
     @Transactional(readOnly = true)
     public Page<Invoice> findAll(Pageable pageable) {
         log.debug("Request to get all Invoices");
-        return invoiceRepository.findAll(pageable);
+        if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+            return invoiceRepository.findAll(pageable);
+        } else {
+            return invoiceRepository.findAllByOrderCustomerUserLogin(SecurityUtils.getCurrentUserLogin().get(), pageable);
+        }
     }
 
 
@@ -60,7 +66,11 @@ public class InvoiceService {
     @Transactional(readOnly = true)
     public Optional<Invoice> findOne(Long id) {
         log.debug("Request to get Invoice : {}", id);
-        return invoiceRepository.findById(id);
+        if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+            return invoiceRepository.findById(id);
+        } else {
+            return invoiceRepository.findOneByIdAndOrderCustomerUserLogin(id, SecurityUtils.getCurrentUserLogin().get());
+        }
     }
 
     /**
