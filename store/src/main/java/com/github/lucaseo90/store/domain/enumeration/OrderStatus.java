3184b222280624078f4e7bb4a4af812e76048b89
Hyunseok Seo <hsseo0501@gmail.com>
2020-08-31 12:04:43 +0900

    Modified store application converted to gateway using JDL

---
+++ store/src/main/java/com/github/lucaseo90/store/domain/enumeration/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.github.lucaseo90.store.domain.enumeration;
+
+/**
+ * The OrderStatus enumeration.
+ */
+public enum OrderStatus {
+    COMPLETED, PENDING, CANCELLED
+}
---
+++ store/src/main/java/com/github/lucaseo90/store/repository/OrderItemRepository.java
@@ -0,0 +1,14 @@
+package com.github.lucaseo90.store.repository;
+
+import com.github.lucaseo90.store.domain.OrderItem;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the OrderItem entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
