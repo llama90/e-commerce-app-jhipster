c80682c9c26cf600f5fb7b4ade8392eac1e6ff43
Hyunseok Seo <hsseo0501@gmail.com>
2020-08-31 11:51:46 +0900

    Added invoice application generated using JDL

---
+++ invoice/src/main/java/com/github/lucaseo90/store/domain/enumeration/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.github.lucaseo90.store.domain.enumeration;
+
+/**
+ * The PaymentMethod enumeration.
+ */
+public enum PaymentMethod {
+    CREDIT_CARD, CASH_ON_DELIVERY, PAYPAL
+}
---
+++ invoice/src/main/java/com/github/lucaseo90/store/repository/ShipmentRepository.java
@@ -0,0 +1,14 @@
+package com.github.lucaseo90.store.repository;
+
+import com.github.lucaseo90.store.domain.Shipment;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Shipment entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+}
